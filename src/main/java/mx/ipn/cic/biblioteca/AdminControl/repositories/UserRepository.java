01a8ddb03d08ad13e77254e4cfc9669a87cc494a
alfredo95 <alfred95esquivel@gmail.com>
2019-05-30 05:16:34 -0500

    funcion del nombre completo ubicado en patientcontroller

--- src/main/java/mx/ipn/cic/biblioteca/AdminControl/repositories/UserRepository.java
+++ src/main/java/mx/ipn/cic/biblioteca/AdminControl/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package mx.ipn.cic.biblioteca.AdminControl.repositories;
 import mx.ipn.cic.biblioteca.AdminControl.model.User;
 import mx.ipn.cic.biblioteca.AdminControl.projections.FindPatientsByEmailResult;
+import mx.ipn.cic.biblioteca.AdminControl.projections.FindFullNameByEmailResult;
 import mx.ipn.cic.biblioteca.AdminControl.repositories.UserBaseRepository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import org.springframework.data.repository.query.Param;
 
 @Transactional
 public interface UserRepository extends UserBaseRepository <User>{
+	
 	@Query(value="SELECT id FROM user WHERE email = :email",nativeQuery = true)
 	public Long findIdByEmail(@Param("email") String email);
 	
@@ -51,7 +53,15 @@ public interface UserRepository extends UserBaseRepository <User>{
 								   @Param("professionalLicense") String professionalLicense, @Param("city") String city,
 								   @Param("hospital") String hospital, @Param("password") String password);
 
-
+	@Transactional
+	@Query(value="SELECT "	+		
+			"u.first_name as nombre, " + 
+			"u.last_namem as apmat, " + 
+			"u.last_namep as appat " + 
+			"FROM user u "+
+			"WHERE u.email = :email",nativeQuery=true)//= :email
+	public List<FindFullNameByEmailResult> findFullNameByEmail(@Param("email") String email);
+	
 //	@Modifying
 //	@Query(value = "UPDATE user u SET u.first_name = :firstName, u.last_namep = :lastNameP, u.last_namem = :lastNameM, " +
 //			"u.email = :email, u.fecha_nac = :birthdate, u.genero = :gender, u.telefono_fijo = :phone, u.telefono_movil = :mobilePhone, " +
