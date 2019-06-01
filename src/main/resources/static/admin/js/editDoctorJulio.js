5537191b2b6dbdeab084975760ead65c7d8488c3
Julio <julio.dzalt@gmail.com>
2019-06-01 18:27:25 -0500

    Corrección editDoctor y editMedico
    Los formularios editDoctor, editMonitor ya no es obligatoria la contraseña

---
+++ src/main/resources/static/admin/js/editDoctorJulio.js
@@ -0,0 +1,92 @@
+/*
+~ Autor:JULIO
+~ Date: 26/05/2019
+
+~ Description: JavaScript for html addDoctor
+*/
+
+$(window).on('load', () => {
+    setTimeout(()=>{
+        $(".orbit-spinner").css({visibility:"hidden",opacity:"0"}).fadeOut(100)
+    }, 2000);
+    setTimeout(()=>{
+        $("#main").removeAttr("hidden").fadeIn(200)
+    }, 2400);
+});
+
+$(window).on('load', () => {
+    document.getElementById("password").value = "";
+});
+
+$(window).ready(() =>{
+    $('#body').bootstrapMaterialDesign();
+    const date = new Date()
+    $('#txtCalendar').dateTimePicker({
+        limitMax: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+    });
+    $('#txtDiagnosisdate').dateTimePicker({
+        limitMax: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+    });
+    $('#txtDateInicioTratamiento').dateTimePicker({
+        limitMin: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`
+    });
+});
+//Aqui
+
+
+
+function Validar (){
+
+    //Julio
+    const btnSave = $('#btnSave');
+    const txtName = $('#txtName');
+    const txtApPaterno = $('#txtApPaterno');
+    const txtApMaterno = $('#txtApMaterno');
+    const txtCalendar = $('#txtCalendar');
+    const slcGener = $('#slcGener');
+    const site = $('#site');
+    const telf = $('#telf');
+    const telm = $('#telm');
+    const cel = $('#cel');
+    const city = $('#city');
+    const email = $('#email');
+    const password = $('#password');
+    const password1 = $('#password1');
+
+    var errores = new String("");
+
+    if (txtName.val()==""||
+        txtApPaterno.val()==""||
+        txtApMaterno.val()==""||
+        slcGener.val()==""||
+        txtCalendar.val()==""||
+        site.val()==""||
+        telf.val()==""||
+        telm.val()==""||
+        cel.val()==""||
+        city.val()==""||
+        email.val()==""//||
+        //password.val()==""||
+        //password1.val()==""
+        )
+    {
+        errores = errores.concat("Todos los campos deben ser llenados obligatoriamente");
+    }
+
+
+
+    if (errores.toString() != "")
+    {
+        var aux = new String("Por favor verifique lo siguiente: \n")
+        aux = aux.concat(errores.toString());
+        swal(aux.toString());
+        return false;
+    }
+    else{
+        swal("Medico Modificado", "", "success");
+        return true;
+
+    }
+
+
+}
